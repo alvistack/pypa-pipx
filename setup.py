0bdecb26a459923e0eb4e7acecde2748af5db2b1
Robert <49005401+Gitznik@users.noreply.github.com>
2024-02-23 21:01:11 +0800

    alvistack/1.7.1
        git clean -xdf
        tar zcvf ../python-pipx_1.7.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-pipx.spec ../python-pipx_1.7.1-1.spec
        cp ../python-pipx*1.7.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/pypa-pipx-1.7.1/
        rm -rf ../python-pipx_1.7.1*.* ../pipx_1.7.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pipx',
+    version='1.7.1',
+    description='Install and Run Python Applications in Isolated Environments',
+    author_email='Chad Smith <chadsmith.software@gmail.com>',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    install_requires=[
+        'argcomplete>=1.9.4',
+        'colorama>=0.4.4; sys_platform == "win32"',
+        'packaging>=20',
+        'platformdirs>=2.1',
+        'tomli; python_version < "3.11"',
+        'userpath!=1.9,>=1.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pipx = pipx.main:cli',
+        ],
+    },
+    packages=[
+        'pipx',
+        'pipx.commands',
+    ],
+    package_dir={'': 'src'},
+)
